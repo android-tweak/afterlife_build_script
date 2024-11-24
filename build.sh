5389df20053238e23f30565c8381def18213bffa
Shahid Parvez <shahidparvez.official@gmail.com>
2024-11-24 21:41:54 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
+#!/bin/bash
 rm -rf .repo/local_manifests/  && # Clone local_manifests repository
 repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs 
- git clone https://github.com/android-tweak/local_manifest_afterlife_rosemary --depth 1 -b main .repo/local_manifests && 
+git clone https://github.com/android-tweak/local_manifest_afterlife_rosemary --depth 1 -b main .repo/local_manifests && 
 # Sync the repositories
 /opt/crave/resync.sh  && 
 # Set up build environment
-export BUILD_USERNAME=mrsnailo 
- export BUILD_HOSTNAME=crave 
- source build/envsetup.sh && 
+export BUILD_USERNAME=mrsnailo
+export BUILD_HOSTNAME=crave 
+source build/envsetup.sh && 
 echo Repository: android-tweak/crave_aosp_builder
- echo Run ID: 11994763277
+echo Run ID: 11994763277
  
 # Build the ROM
 goafterlife rosemary
